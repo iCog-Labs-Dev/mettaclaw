d6110013b6d33e703d4bfded73cc32f1f97f9d99
CodersKin <nebiyuhiyabu@gmail.com>
2026-07-16 13:23:20 +0300

    Fix: Fixed outdated websearch code

---
+++ src/websearch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from ddgs import DDGS
+
+def search_(query, max_results=10):
+    with DDGS() as ddgs:
+        return [
+            {
+                "title": r.get("title", ""),
+                "url": r.get("href", ""),
+                "snippet": r.get("body", "")
+            }
+            for r in ddgs.text(query, max_results=max_results)
+        ]
+
+def search(query, max_results=10):
+    try:
+        ret = "("
+        for r in search_(query):
+            ret += "(TITLE: " + r["title"] + " SNIPPET: " + r["snippet"] + ") "
+        ret += ")"
+        return ret
+    except Exception:
+        return ""
